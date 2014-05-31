81fe8dd30e98b6d33a404701408690470e31efa1
apelttom <t.apeltauer@seznam.cz>
2014-05-31 16:06:30 -0600

    UPDATE: WinBGIm integration
    Changed the project, so it uses WinBGIm library to draw graphics.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <winbgim.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Labyrint application" << endl;
+    //cout << "Labyrint application" << endl;
+    initwindow (600,400);
+    getch();
+    closegraph();
     return 0;
 }
